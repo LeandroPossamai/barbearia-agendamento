539596fe51050dcea40e7f6f67f2670fe6eefab9
Leandro Possamai <mathlee010203@gmail.com>
2024-06-07 17:24:37 -0300

    commit da imagem de fundo e o login um pouco mas estilizado

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -13,31 +13,33 @@ const Login = () => {
     };
 
     return (
-        <div className="login-container" style={{backgroundImage: 'url(/public/images/barbearia.jpg)'}}>
+        <div className="login-container">
             <form className="login-form" onSubmit={handleSubmit}>
                 <h2>Login</h2>
                 {error && <p className="error-message">{error}</p>}
                 <div className="form-group">
-    <FaEnvelope className="input-icon" />
-    <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-    />
-</div>
-<div className="form-group">
-    <FaLock className="input-icon" />
-    <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-    />
-</div>
-
+                    <FaEnvelope className="input-icon" />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <FaLock className="input-icon" />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="login-button-container">
+                    <button type="submit" className="login-button">Login</button>
+                </div>
             </form>
         </div>
     );
